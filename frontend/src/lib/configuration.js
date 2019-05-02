c699b50cf5875f8f33a7651022ddaf6b915e0b63
Alessio Coser <alessio.coser@xpeppers.com>
2019-05-02 18:11:19 +0200

    updates api domain name

--- frontend/src/lib/configuration.js
+++ frontend/src/lib/configuration.js
@@ -1,3 +1,3 @@
 module.exports = {
-  apiBaseUrl: process.env.REACT_APP_BASE_URL || 'https://3ehy0jf96k.execute-api.us-east-1.amazonaws.com/dev'
+  apiBaseUrl: process.env.REACT_APP_BASE_URL || 'https://api.academy.xpeppers.com'
 }
