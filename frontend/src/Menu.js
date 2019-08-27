46428b9200f01b4cc0cc689ca856d0f03f8cbdb9
Alessio Coser <alessio.coser@xpeppers.com>
2019-08-27 14:26:35 +0200

    convert menu page to functional

--- frontend/src/Menu.js
+++ frontend/src/Menu.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-class Menu extends Component {
-    render() {
-        const pages = [
-            { code: "list", name: "List", url: "/" },
-            { code: "ranking", name: "Ranking", url: "/ranking" },
-            { code: "add", name: "+ Add", url: "/add" }
-        ];
+const Menu = (props) => {
+  const pages = [
+    { code: "list", name: "List", url: "/" },
+    { code: "ranking", name: "Ranking", url: "/ranking" },
+    { code: "add", name: "+ Add", url: "/add" }
+  ]
 
-        return (
-            <ul className="nav">
-                {pages.map((page, index) =>
-                    <li key={'page' + index} className={"nav-item " + (page.code === this.props.selected ? 'active' : '')}>
-                        <Link to={page.url} className="nav-link">{page.name}</Link>
-                    </li>
-                )}
-            </ul>
-        )
-    }
+  return (
+    <ul className="nav">
+      {pages.map((page, index) =>
+        <li key={'page' + index} className={"nav-item " + (page.code === props.selected ? 'active' : '')}>
+          <Link to={page.url} className="nav-link">{page.name}</Link>
+        </li>
+      )}
+    </ul>
+  )
 }
 
-export default Menu;
+export default Menu
